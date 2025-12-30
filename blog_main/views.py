0075d0a59607b021d44898380034ed453a0928be
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2025-12-30 23:30:42 +0530

    Add blogpage and search functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from assignments.models import About
 from blogs.models import Blog, Category
 
 
@@ -7,9 +8,14 @@ def home(request):
     # categories=Category.objects.all()
     featured_posts=Blog.objects.filter(is_featured=True,status='Published')
     posts=Blog.objects.filter(is_featured=False,status='Published')
+    try:
+        about=About.objects.get()
+    except:
+        about=None
     context={
         # 'categories':categories,
         'featured_posts':featured_posts,
         'posts':posts,
+        'about':about,
     }
     return render(request,'home.html',context)
