92ca8634f57543e60eb46033080f09814191ed49
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2025-12-30 12:31:55 +0530

    Blog app and admin pannel models display

---
+++ blogs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Blog, Category
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+    list_display=('id','title','category','status','is_featured')
+    search_fields=('title','category__category_name','status')
+    list_editable=('is_featured',)
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
