809d7da6fe932a1648cf2af874d782b8ae8fab69
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2026-01-02 21:17:38 +0530

    Add Category Crud functionality in dashboard

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from blogs import views as Blogviews
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
@@ -16,4 +17,6 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login_view,name='login'),
     path('logout',views.logout_view,name='logout'),
+    #dashboard
+    path('dashboard/',include('dashboard.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -48,7 +48,7 @@ def login_view(request):
             user=authenticate(username=username,password=password)
             if user is not None:
                 login(request,user)
-                return redirect('home')
+                return redirect('dashboard')
 
     form=AuthenticationForm()
     context={
