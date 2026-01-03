46b850029dd1c6aab48ae40cf01d364793621ed5
Miryabbelli Tarun <miryabbellitarun@gmail.com>
2026-01-03 15:49:51 +0530

    Add post edit page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('categories/delete/<int:pk>/',views.delete_category,name='delete_category'),
     #posts
     path('posts/',views.posts,name='posts'),
-    path('posts/add',views.add_post,name='add_post'),
+    path('posts/add/',views.add_post,name='add_post'),
+    path('posts/edit/<int:pk>/',views.edit_post,name='edit_post'),
 ]
